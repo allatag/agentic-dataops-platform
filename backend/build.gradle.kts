dd3be89d10cd2840f93ab31feb0da705d610053b
allatag <t.allayarov@gmail.com>
2026-06-08 23:02:30 +0200

    Fix Flyway PostgreSQL startup

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -26,6 +26,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.kafka:spring-kafka")
 	implementation("org.flywaydb:flyway-core")
+	implementation("org.flywaydb:flyway-database-postgresql")
 	implementation("org.postgresql:postgresql")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
